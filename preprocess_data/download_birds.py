d42ee6de758ceeaaa56f4c6b1d6a28feafc2481a
M Nauta <40458104+M-Nauta@users.noreply.github.com>
2021-05-05 10:28:07 +0200

    Update download_birds.py

--- preprocess_data/download_birds.py
+++ preprocess_data/download_birds.py
@@ -4,7 +4,7 @@ import os
 import gdown
 # url = 'http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz'
 url = 'https://drive.google.com/uc?id=1hbzc_P1FuxMkcabkgn9ZKinBwW683j45' 
-target_path = '../data/CUB_200_2011/CUB-200-2011.tgz'
+target_path = './data/CUB_200_2011/CUB-200-2011.tgz' #you might need to change the name to CUB_200_2011.tgz
 
 print(os.path.abspath(target_path))
 
@@ -12,9 +12,10 @@ print(os.path.abspath(target_path))
 # if response.status_code == 200:
 #     with open(target_path, 'wb') as f:
 #         f.write(response.raw.read())
-#or download it manually and uncomment this part
+
+#or download it manually and uncomment the line below
 gdown.download(url, target_path, quiet=False)
-# 
+ 
 tar = tarfile.open(target_path, "r:gz")
 tar.extractall(path='./data')
 tar.close()
